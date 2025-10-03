947ff6572ab987ca115a16414ffa01daba703643
coschul <cohenschulz1204@gmail.com>
2025-10-03 14:06:00 -0400

    password now opens the dashboard page. Logic is a bit broken: need to do more research into session to make sure that the user cannot just enter the url to gain access

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,5 +1,5 @@
 # flask requirements
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import hashlib
 
 # database requirements
@@ -10,27 +10,32 @@ from flaskapp import database as db
 # ===== Flask Initialization =====
 
 app = Flask(__name__)
-
-
+app.secret_key = db.get_secret_key()
 
 # ===== Routes =====
 
 @app.route("/")
 def render_index():
+    session['logged_in'] = False
     return render_template("index.html")
 
-@app.route("/home")
-def home():
-    return render_template("home.html")
+@app.route("/dashboard")
+def dashboard():
+    if 'logged_in' in session and session['logged_in']:
+        return render_template("dashboard.html")
+    else:
+        return redirect(url_for('login'))
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    password = request.form.get("password")
-    hashed_password = hashlib.sha256(password.encode()).hexdigest()
-
-    if db.validate_password(hashed_password):
-        print("Password is valid!")
+    if request.method == "GET":
+        return render_template("login.html")
     else:
-        print("Invalid password.")
-        
-    return render_template("home.html")
+        password = request.form.get("password")
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+        if db.validate_password(hashed_password):
+            session['logged_in'] = True
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template("login.html")
--- flaskapp/database.py
+++ flaskapp/database.py
@@ -3,3 +3,7 @@ def validate_password(password):
     with open("flaskapp/secret.txt", "r") as f:
         stored_password_hash = f.read().strip()
     return password == stored_password_hash
+
+def get_secret_key():
+    with open("flaskapp/secret_key.txt", "r") as f:
+        return f.read().strip()
